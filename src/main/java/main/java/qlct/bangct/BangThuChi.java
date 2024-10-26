1be0e8cea33e73fe767c90dce69ad5f60ca8c85b
dovie <dovie@DESKTOP-IAPMV7O>
2024-10-26 14:12:38 +0700

    (23010547) Code tren thiet bi khac

---
+++ src/main/java/main/java/qlct/bangct/BangThuChi.java
@@ -0,0 +1,20 @@
+package main.java.qlct.bangct;
+
+public class BangThuChi {
+    public int thang;
+    public int ngay;
+    public int nam;
+    public String ndtc;
+    public String type;
+    public int value;
+    
+    public BangThuChi(int thang,int ngay,int nam,String ndtc,String type,int value){
+        this.thang = thang;
+        this.ngay = ngay;
+        this.nam = nam;
+        this.ndtc = ndtc;
+        this.type = type;
+        this.value = value;
+    }
+    
+}
